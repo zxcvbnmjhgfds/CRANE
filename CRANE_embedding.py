2bc708eb3138d0d6dd48fd0803320dde496b9062
zxcvbnmjhgfds <101302101+zxcvbnmjhgfds@users.noreply.github.com>
2022-03-10 16:52:13 +0800

    Add files via upload

--- CRANE_embedding.py
+++ CRANE_embedding.py
@@ -1,3 +1,249 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:585a7f7d0b72d659009464f56d64d6803d29f96e219bb2a5518fe4bb096675c1
-size 12991
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.init import xavier_normal_
+from collections import defaultdict
+import math
+import pickle
+import random
+import argparse
+
+class InequalityEmbedding(nn.Module):
+
+    def __init__(self, poi_num, cbg_num, percentile_num, embedding_dim, device='cpu'):
+        # poi_num: int, number of POIs in the MSA
+        # cbg_num: int, numebr of CBGs in the MSA
+        # percentile_num: int, number of treatment levels to learn embedding
+        # embedding_dim: int
+        super(InequalityEmbedding, self).__init__()
+        self.device = device
+        self.category_num = 4 # 4 categories
+        self.poi_num = poi_num
+        self.cbg_num = cbg_num
+        self.feature_num = 5 # 5 demographic features
+        self.percentile_num = percentile_num
+        self.embedding_dim = embedding_dim
+        self.cate_embeddings = nn.Embedding(num_embeddings=4, embedding_dim=embedding_dim).to(device)
+        self.poi_embeddings = nn.Embedding(num_embeddings=poi_num, embedding_dim=embedding_dim).to(device)
+        self.cbg_embeddings = nn.Embedding(num_embeddings=cbg_num, embedding_dim=embedding_dim).to(device)
+        self.percentile_embeddings = nn.Embedding(num_embeddings=5 * self.percentile_num, embedding_dim=embedding_dim).to(device)
+    
+    def pick_feat(self, feature_id, feature_val):
+        # center of each treatment level, [10%, 30%, 50%, 70%, 90%] for percentile_num = 5
+        percentiles = torch.tensor([1/self.percentile_num/2+i/self.percentile_num for i in range(self.percentile_num)], device=self.device)
+        feature_val = torch.unsqueeze(feature_val,1)
+        # weights assigned to each treatment level
+        m = F.softmax(-torch.abs(self.percentile_num * (feature_val - percentiles)), dim=1)
+        # embedding of a specific feature_val
+        feat_emb = torch.zeros((feature_val.shape[0], self.embedding_dim), device=self.device)
+        for i in range(self.percentile_num):
+            curr_emb = self.percentile_embeddings(torch.tensor(feature_id * self.percentile_num + i + 0*feature_val.squeeze()).long())
+            feat_emb += m[:, i:i+1] * curr_emb
+        return feat_emb
+
+    def init(self):
+        # initialize embedding vectors
+        xavier_normal_(self.cate_embeddings.weight.data)
+        xavier_normal_(self.poi_embeddings.weight.data)
+        xavier_normal_(self.cbg_embeddings.weight.data)
+        xavier_normal_(self.percentile_embeddings.weight.data)
+
+    def regularization_feat(self):
+        # demographic regularization term: sum of square distance between adjacent treatment level embeddings
+        l = torch.tensor(0, dtype=torch.float32).to(self.device)
+        for f in range(self.feature_num):
+            for i in range(self.percentile_num-1):
+                ff = torch.tensor(f, dtype=torch.long).to(self.device)
+                ii = torch.tensor(i, dtype=torch.long).to(self.device)
+                a0 = self.percentile_embeddings(ff*self.percentile_num+ii)
+                a1 = self.percentile_embeddings(ff*self.percentile_num+ii+1)
+                l += torch.sum(torch.square(a1-a0))
+        return l
+
+    def regularization_CBG(self, CBG_neighbors):
+        # spatial regularization term: weighted sum of square distance between adjacent CBG's embeddings
+        c1 = CBG_neighbors[:, 0].long()
+        c2 = CBG_neighbors[:, 1].long()
+        d = CBG_neighbors[:, 2]
+        a0 = self.cbg_embeddings(c1)
+        a1 = self.cbg_embeddings(c2)
+        return torch.sum(torch.sum(torch.square(a1-a0), 1)*d)
+        
+    def forward(self, inputs):
+        # embedding loss
+        cate_id = inputs[:, 0].long()
+        poi_id = inputs[:, 1].long()
+        cbg_id = inputs[:, 2].long()
+        feature0 = inputs[:, 3]
+        feature1 = inputs[:, 4]
+        feature2 = inputs[:, 5]
+        feature3 = inputs[:, 6]
+        feature4 = inputs[:, 7]
+        altcbg0 = inputs[:, 8].long()
+        altfeature0 = inputs[:, 9]
+        altcbg1 = inputs[:, 10].long()
+        altfeature1 = inputs[:, 11]
+        altcbg2 = inputs[:, 12].long()
+        altfeature2 = inputs[:, 13]
+        altcbg3 = inputs[:, 14].long()
+        altfeature3 = inputs[:, 15]
+        altcbg4 = inputs[:, 16].long()
+        altfeature4 = inputs[:, 17]
+        cate_embed = self.cate_embeddings(cate_id)
+        poi_embed = self.poi_embeddings(poi_id)
+        cbg_embed = self.cbg_embeddings(cbg_id)
+        observed_embed0 = self.pick_feat(0, feature0)
+        observed_embed1 = self.pick_feat(1, feature1)
+        observed_embed2 = self.pick_feat(2, feature2)
+        observed_embed3 = self.pick_feat(3, feature3)
+        observed_embed4 = self.pick_feat(4, feature4)
+        altcbg_embed0 = self.cbg_embeddings(altcbg0)
+        altcbg_embed1 = self.cbg_embeddings(altcbg1)
+        altcbg_embed2 = self.cbg_embeddings(altcbg2)
+        altcbg_embed3 = self.cbg_embeddings(altcbg3)
+        altcbg_embed4 = self.cbg_embeddings(altcbg4)
+        altfeat_embed0 = self.pick_feat(0, altfeature0)
+        altfeat_embed1 = self.pick_feat(1, altfeature1)
+        altfeat_embed2 = self.pick_feat(2, altfeature2)
+        altfeat_embed3 = self.pick_feat(3, altfeature3)
+        altfeat_embed4 = self.pick_feat(4, altfeature4)
+        
+        loss_cate_cbg = -F.logsigmoid(torch.sum(cate_embed * cbg_embed, dim=1)) \
+            - 0.2 * F.logsigmoid(-torch.sum(cate_embed * altcbg_embed0, dim=1)) \
+            - 0.2 * F.logsigmoid(-torch.sum(cate_embed * altcbg_embed1, dim=1)) \
+            - 0.2 * F.logsigmoid(-torch.sum(cate_embed * altcbg_embed2, dim=1)) \
+            - 0.2 * F.logsigmoid(-torch.sum(cate_embed * altcbg_embed3, dim=1)) \
+            - 0.2 * F.logsigmoid(-torch.sum(cate_embed * altcbg_embed4, dim=1))
+        loss_cate_feat = -F.logsigmoid(torch.sum(cate_embed * observed_embed0, dim=1)) \
+            -F.logsigmoid(torch.sum(cate_embed * observed_embed1, dim=1)) \
+            -F.logsigmoid(torch.sum(cate_embed * observed_embed2, dim=1)) \
+            -F.logsigmoid(torch.sum(cate_embed * observed_embed3, dim=1)) \
+            -F.logsigmoid(torch.sum(cate_embed * observed_embed4, dim=1)) \
+            - F.logsigmoid(-torch.sum(cate_embed * altfeat_embed0, dim=1)) \
+            - F.logsigmoid(-torch.sum(cate_embed * altfeat_embed1, dim=1)) \
+            - F.logsigmoid(-torch.sum(cate_embed * altfeat_embed2, dim=1)) \
+            - F.logsigmoid(-torch.sum(cate_embed * altfeat_embed3, dim=1)) \
+            - F.logsigmoid(-torch.sum(cate_embed * altfeat_embed4, dim=1))
+        loss_poi_cbg = -F.logsigmoid(torch.sum(poi_embed * cbg_embed, dim=1)) \
+            - 0.2 * F.logsigmoid(-torch.sum(poi_embed * altcbg_embed0, dim=1)) \
+            - 0.2 * F.logsigmoid(-torch.sum(poi_embed * altcbg_embed1, dim=1)) \
+            - 0.2 * F.logsigmoid(-torch.sum(poi_embed * altcbg_embed2, dim=1)) \
+            - 0.2 * F.logsigmoid(-torch.sum(poi_embed * altcbg_embed3, dim=1)) \
+            - 0.2 * F.logsigmoid(-torch.sum(poi_embed * altcbg_embed4, dim=1))
+        loss_poi_feat = -F.logsigmoid(torch.sum(poi_embed * observed_embed0, dim=1)) \
+            -F.logsigmoid(torch.sum(poi_embed * observed_embed1, dim=1)) \
+            -F.logsigmoid(torch.sum(poi_embed * observed_embed2, dim=1)) \
+            -F.logsigmoid(torch.sum(poi_embed * observed_embed3, dim=1)) \
+            -F.logsigmoid(torch.sum(poi_embed * observed_embed4, dim=1)) \
+            - F.logsigmoid(-torch.sum(poi_embed * altfeat_embed0, dim=1)) \
+            - F.logsigmoid(-torch.sum(poi_embed * altfeat_embed1, dim=1)) \
+            - F.logsigmoid(-torch.sum(poi_embed * altfeat_embed2, dim=1)) \
+            - F.logsigmoid(-torch.sum(poi_embed * altfeat_embed3, dim=1)) \
+            - F.logsigmoid(-torch.sum(poi_embed * altfeat_embed4, dim=1))
+        return torch.sum(loss_cate_cbg)+torch.sum(loss_cate_feat)+torch.sum(loss_poi_cbg)+torch.sum(loss_poi_feat)
+
+# set random seeds
+def setup_seed(seed):
+     torch.manual_seed(seed)
+     torch.cuda.manual_seed_all(seed)
+     np.random.seed(seed)
+     random.seed(seed)
+     torch.backends.cudnn.deterministic = True
+
+# prepare data for spatial regularization
+def prepare_spatial_regularization(city_id, threshold, device):
+    dists = np.load('data/CBG_dists_'+str(city_id)+'.npy')
+    a, b = np.where(dists < threshold)
+    CBG_neighbors = np.array([np.array([a[i], b[i], dists[a[i]][b[i]]]) for i in range(len(a)) if a[i] < b[i]])
+    def kernel(x, threshold):
+        return np.exp(-np.square(x)/2/np.square(threshold))/2/np.pi/np.square(threshold)
+    CBG_neighbors[:, 2] = kernel(CBG_neighbors[:, 2], threshold)
+    CBG_neighbors = torch.tensor(CBG_neighbors).to(device)
+    return CBG_neighbors
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-embedding_dim', default = 16, type=int) # dimension of embedding vectors
+    parser.add_argument('-batch_size', default=512, type=int) # batch size
+    parser.add_argument('-city_id', default = 1, type=int) # MSA ID, 1: New York, 2: Los Angeles, 3: Chicago
+    parser.add_argument('-cuda', default = 6, type=int) # GPU device
+    parser.add_argument('-seed', default = 1999, type=int) # random seed
+    parser.add_argument('-percentile_num', default = 5, type=int) # number of treatment levels to learning embedding
+    parser.add_argument('-threshold', default = 2.5, type=float) # spatial threshold
+    parser.add_argument('-l2', default = 0.01, type=float) # strength of demographic regulariztion
+    parser.add_argument('-l2_CBG', default = 0.01, type=float) # strength of spatial regularization
+    args = parser.parse_args()
+    
+    setup_seed(args.seed)  
+    device = torch.device('cuda:'+str(args.cuda))
+    
+    # Mapping CBG ID & POI ID to embedding IDs
+    with open('data/cbgid_embeddingid_mapping.pkl', 'rb') as f:
+        cbgid_dict = pickle.load(f)
+    with open('data/poiid_embeddingid_mapping.pkl', 'rb') as f:
+        poiid_dict = pickle.load(f)
+    poiid_mapping = poiid_dict[args.city_id]
+    poi_num = len(poiid_mapping)
+    cbgid_mapping = cbgid_dict[args.city_id]
+    cbg_num = len(cbgid_mapping)
+    del poiid_dict
+    del cbgid_dict
+
+    dataset = torch.load('data/dataset'+str(args.city_id)+'.pt', map_location=device)
+    lendata = dataset.size()[0]
+    CBG_neighbors = prepare_spatial_regularization(args.city_id, args.threshold, device)
+    
+    losses = []
+    model = InequalityEmbedding(poi_num, cbg_num, args.percentile_num, args.embedding_dim, device)
+    model.to(device)
+    model.init()
+    l2 = torch.tensor(args.l2).to(device)
+    l2_CBG = torch.tensor(args.l2_CBG).to(device) 
+    optimizer = torch.optim.Adam(model.parameters(), 0.001)
+    epochs = 10001
+    print("epoch\t loss\t")
+    weights = {}
+    batch_size = args.batch_size
+
+    # train embeddings
+    for epoch in range(epochs):
+        total_loss = 0
+        sim_loss = 0
+        feat_loss = 0
+        CBG_loss = 0
+        for i in range(math.ceil(lendata/batch_size)):
+            data = dataset[i*batch_size:(i+1)*batch_size]
+            optimizer.zero_grad()
+            # embedding loss
+            loss_similarity = model(data)
+            # demographic regularization
+            loss_feat_regularization = model.regularization_feat() * data.size()[0]
+            # spatial regularization
+            loss_CBG_regularization = model.regularization_CBG(CBG_neighbors) * data.size()[0]
+            loss = loss_similarity + l2 * loss_feat_regularization + l2_CBG * loss_CBG_regularization
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item() / dataset.size()[0]
+            sim_loss += loss_similarity.item() / dataset.size()[0]
+            feat_loss += loss_feat_regularization.item() / dataset.size()[0]
+            CBG_loss += loss_CBG_regularization.item() / dataset.size()[0]
+        if epoch % 50 == 0:
+            print(epoch, total_loss, sim_loss, feat_loss, CBG_loss)
+        if epoch % 1000 == 0:
+            # save embedding vectors every 1000 epoch
+            weights[epoch] = [model.cate_embeddings.weight.to('cpu').clone().detach(), model.poi_embeddings.weight.to('cpu').clone().detach(), model.cbg_embeddings.weight.to('cpu').clone().detach(), model.percentile_embeddings.weight.to('cpu').clone().detach()]
+        losses.append(total_loss)
+
+    write_dict = {}
+    write_dict['weights'] = weights
+    write_dict['losses'] = losses
+    write_dict['args'] = args
+
+    outfilename = 'data/embedding_city'+str(args.city_id)+'dim'+str(args.embedding_dim)+'batch'+str(args.batch_size)+'seed'+str(args.seed)+'percentile'+str(args.percentile_num)+'threshold'+str(args.threshold)+'l2'+str(args.l2)+'l2_CBG'+str(args.l2_CBG)+'.pkl'
+
+    with open(outfilename, 'wb') as f:
+        pickle.dump(write_dict, f)
+
--- CounterfactualRandomWalk.py
+++ CounterfactualRandomWalk.py
@@ -1,3 +1,169 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cb92054de4305f0ca5da1890629a0e5dea67b16413211cd33efb37ad04aeda52
-size 8357
+import numpy as np
+import pandas as pd
+import pickle
+import random
+import torch
+from torch.nn.functional import softmax
+from collections import defaultdict
+import argparse
+
+# transform random walks to torch datasets
+def transform_dataset(sample_dict, city_id):
+    with open('./data/cbg_locate_in_msa_dict.pkl', 'rb') as f:
+        cbg_locate_in_msa_dict = pickle.load(f)
+    cbgs = [x for x in cbg_locate_in_msa_dict.keys() if cbg_locate_in_msa_dict[x] == city_id]
+    # CBG features
+    df = pd.read_csv('./data/middle.csv')
+    df = df[df.census_block_group.isin(cbgs)]
+    df = df[df['Total population'] > 100]
+    df = df[['census_block_group', 'female_ratio', 'white_ratio', 'bachelor_ratio', 'average_income', 'population_young_ratio', 'population_old_ratio', 'disability_household_ratio']]
+    df = df.dropna()
+    features = ['female_ratio', 'white_ratio', 'bachelor_ratio','average_income', 'disability_household_ratio']
+    
+    def interpret(x):
+        # category_id, POI_id, observed CBG_id
+        g = [x[0]-1, x[1]-1, x[2]-1]
+        # percentile of 5 observed outcome 
+        for k in range(5):
+            temp = df[features[k]]
+            g.append(np.sum(temp <= x[3+k]) / len(df))
+        for k in range(5):
+            # alternative CBG_id
+            g.append(x[8+2*k]-1)
+            temp = df[features[k]]
+            # percentile of alternative outcome 
+            g.append(np.sum(temp <= x[9+2*k]) / len(df))
+        return g
+    dataset = []
+    for cate in range(1, 5):
+        dataset += [interpret(x) for x in sample_dict[cate]]
+    random.shuffle(dataset)
+    dataset = torch.tensor(dataset)
+    torch.save(dataset, './data/dataset'+str(city_id)+'.pt')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-poi_num', default = 1, type=int) # Number of POIs sampled from the category in each iteration
+    parser.add_argument('-city_id', default = 1, type=int) # MSA ID, 1: New York, 2: Los Angeles, 3: Chicago
+    parser.add_argument('-cuda', default = 4, type=int) # CUDA device ID
+    parser.add_argument('-q', default=5, type=int) # Number of covariate's strata
+    args = parser.parse_args()
+    
+    # Load ID of CBGs located in the MSA
+    with open('./data/cbg_locate_in_msa_dict.pkl', 'rb') as f:
+        cbg_locate_in_msa_dict = pickle.load(f)
+    city_cbgs = [x for x in cbg_locate_in_msa_dict.keys() if cbg_locate_in_msa_dict[x] == args.city_id]
+    del cbg_locate_in_msa_dict
+
+    # Load demographic features of CBGs located in the MSA
+    df = pd.read_csv('./data/middle.csv')
+    df = df[df.census_block_group.isin(city_cbgs)]
+    df = df[['census_block_group', 'female_ratio', 'white_ratio', 'bachelor_ratio', 'average_income', 'population_young_ratio', 'population_old_ratio', 'disability_household_ratio']]
+    df = df.dropna()
+    CBG_record = list(df.census_block_group)
+
+    # Visit patterns (CBG-POI network) in the MSA
+    # visit_each_poi_new: dict
+    # keys: POI ID (str, 'sg:4433120bada9409da78492d31f6bbc9e')
+    # values: (category ID (int, 1~4), CBG visit dictionary(dict))
+    # CBG visit dictionary: 
+    # keys: CBG ID (int, 481576747003)
+    # values: CBG's urban resource accessibility to the POI 
+    with open('./data/visit_each_poi_new_'+str(args.city_id)+'.pkl', 'rb') as f:
+        visit_each_poi_new = pickle.load(f)
+    
+    # Mapping CBG ID & POI ID to embedding IDs
+    with open('./data/cbgid_embeddingid_mapping.pkl', 'rb') as f:
+        cbgid_dict = pickle.load(f)
+    with open('./data/poiid_embeddingid_mapping.pkl', 'rb') as f:
+        poiid_dict = pickle.load(f)
+    cbgid_dict = cbgid_dict[args.city_id]
+    poiid_dict = poiid_dict[args.city_id]
+
+    poi_by_cate = dict()
+    for cate in range(1, 5):
+        # all POIs within the category
+        poi_in_cate = [x for x in visit_each_poi_new.keys() if visit_each_poi_new[x][0] == cate and poiid_dict[x] > 0]
+        # sum of CBG's urban resource accessibility to each POI
+        poi_num_dict = [sum(visit_each_poi_new[x][1].values()) for x in poi_in_cate]
+        poi_by_cate[cate] = (poi_in_cate, poi_num_dict)
+
+    # sample POIs from a category Q based on POI's total urban resource accessibility
+    def sample_poi(cate, num=1, cu='cuda:7'):
+        device = torch.device(cu)
+        a, prob = poi_by_cate[cate]
+        prob = torch.tensor(prob, device=device)
+        prob = prob / torch.sum(prob)
+        idx = prob.multinomial(num_samples=num,  replacement = True).to('cpu')
+        return np.array(a)[idx.numpy()]
+    
+    # sample a CBG from all CBGs that have visited a POI based on their urban resource accessibility to the POI
+    def sample_cbg_normalized(poi_id, num=1, cu='cuda:7'):
+        device = torch.device(cu)
+        visit_dict = visit_each_poi_new[poi_id][1]
+        a = [x for x in list(visit_dict.keys()) if cbgid_dict[x] > 0]
+        if len(a) == 0:
+            return None
+        prob = np.array([visit_dict[x] for x in a])
+        prob = torch.tensor(prob, device=device)
+        prob = prob / torch.sum(prob)
+        idx = prob.multinomial(num_samples=num, replacement = True).to('cpu')
+        return np.array(a)[idx.numpy()]
+
+    features = ['female_ratio', 'white_ratio', 'bachelor_ratio', 'average_income',
+    'disability_household_ratio', 'population_young_ratio', 'population_old_ratio']
+    # stratified covariates to be matched when sampling alternative outcome
+    def confounder(f):
+        return ['binned_'+x for x in features if x != f]
+    # stratify demographic features
+    for f in features:
+        binned_name = 'binned_'+f
+        _, bins = np.array(pd.qcut(df[f], q=args.q, duplicates='drop', retbins = True))
+        df[binned_name] = np.array(pd.qcut(df[f], q=args.q, labels = list(range(len(bins)-1)),duplicates='drop', retbins = False))
+    
+    # sample observed and alternative outcome
+    def sample_feature(cbg_id, poi_id, feat, cu='cuda:7'):
+        visit_dict = visit_each_poi_new[poi_id][1]
+        # all CBGs that have visited the POI
+        visited_cbgs = [x for x in list(visit_dict.keys()) if cbgid_dict[x] > 0]
+        # observed outcome and covariates of the observed CBG
+        observed_outcome = df[df.census_block_group == cbg_id][feat].values[0]
+        conf = df[df.census_block_group == cbg_id][confounder(feat)].values[0]
+        # sample an alternative CBG from CBGs that have not visited the POI
+        middle = df[~df.census_block_group.isin(visited_cbgs)]
+        vals = np.array(list(middle[feat]))
+        cbgs = list(middle['census_block_group'])
+        # select the alternative CBG based on discrepancy in stratified covariates
+        dists = np.sum(np.square(np.array(middle[confounder(feat)]) - conf), axis=1)
+        prob = torch.tensor(dists, dtype=torch.float64, device=cu)
+        idx = softmax(-10*prob, dim=0).multinomial(num_samples=1,replacement = True).to('cpu')
+        alternative_outcomes = vals[idx.numpy()]
+        alternative_cbg = [cbgs[i] for i in idx.numpy()]
+        # observed CBG, observed outcome, alternative CBG, alternative outcome
+        return cbg_id, observed_outcome, alternative_cbg[0], alternative_outcomes[0]
+
+    # category IDs: 
+    # 1: Art & Recreation, 2: Sports, 3: Education, 4: Health
+    sample_dict = {1: [], 2: [], 3:[], 4:[]}
+    for cate in range(1, 5):
+        sampled_POIs = sample_poi(cate, num=args.poi_num, cu='cuda:'+str(args.cuda))
+        for poi in sampled_POIs:
+            CBGs = sample_cbg_normalized(poi, num=1, cu='cuda:'+str(args.cuda))
+            for cbg_id in CBGs:
+                if cbg_id not in CBG_record:
+                    continue
+                sample = [cate, poiid_dict[poi], cbgid_dict[cbg_id]]
+                alts = []
+                # sample an alternative CBG for each of the five demographic features
+                for feat in features[:5]:
+                    _, observed_outcome, alt_cbg, alt_outcome = sample_feature(cbg_id, poi, feat, cu='cuda:'+str(args.cuda))
+                    sample.append(observed_outcome)
+                    alts.append(cbgid_dict[alt_cbg])
+                    alts.append(alt_outcome)
+                sample_dict[cate].append(sample+alts)
+
+    outfilename = './data/CounterfactualRandomWalk_'+str(args.city_id)+'_poinum'+str(args.poi_num)+'.pkl'
+    with open(outfilename, 'wb') as f:
+        pickle.dump(sample_dict, f)
+    
+    transform_dataset(sample_dict, args.city_id)
